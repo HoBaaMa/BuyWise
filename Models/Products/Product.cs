bb27e860b8e205afc32c02179152fde21e378bf5
ehab <ehab.sadiq89@gmail.com>
2025-05-10 15:58:26 +0300

    Cleanup unused code in Product class
    Removed commented-out constructor and ProductStock record
    definition in the BuyWise.Models.Products namespace to
    streamline the Product class.

--- Models/Products/Product.cs
+++ Models/Products/Product.cs
@@ -7,20 +7,6 @@ namespace BuyWise.Models.Products
     public class Product : BaseModel
     {
 
-
-        public Product()
-        {
-            //List<ProductStock> productQty = new();
-
-            //ProductStock p1 = new(2, 0);
-            //productQty.Add(p1);
-            //ProductStock p2 = new(3, 10);
-            //productQty.Add(p2);
-            //ProductStock p3 = new(4, 12);
-            //productQty.Add(p1);
-            //ProductStock p4 = new(5, 0);
-            //productQty.Add(p2);
-        }
         public string? ShortDescription { get; set; }
         public string? ArabicName { get; set; }
         public string? Description { get; set; }
