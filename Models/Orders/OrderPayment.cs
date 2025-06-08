5f4203f601435fb37886866bf94f3570db3b8d8e
Ehab Sadiq <125506037+HoBaaMa@users.noreply.github.com>
2025-06-08 21:58:17 +0300

    Rename and fix order payment model

--- Models/Orders/OrderPayment.cs
+++ Models/Orders/OrderPayment.cs
@@ -2,12 +2,12 @@
 
 namespace BuyWise.Models.Orders
 {
-    public class OrderPaymnet
+    public class OrderPayment
     {
         public long Id { get; set; }
         public long OrderId { get; set; }
         public Order? Order { get; set; }
         public long PaymentMethodId { get; set; }
-        public PaymentMehod? PaymentMehod { get; set; }
+        public PaymentMehod? PaymentMethod { get; set; }
     }
 }
